83604b533c27e01de58fa097f2addb443ed31f08
Yas-CF <yasna.kcf@gmail.com>
2024-12-11 01:47:33 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,6 +5,5 @@
  curl -sL https://rpm.nodesource.com/setup_16.x | sudo bash -
  sudo yum install -y nodejs
  sudo yum install -y coreutils
- cd /home/ec2-user/app
  npm install
  nohup node app.js > app.log 2>&1 &"##
